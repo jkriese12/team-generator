9b934c869f7c597eb9f5d6cca842f20d6a9567f2
Jacob Kriese <jkriese12@gmail.com>
2021-01-30 16:07:54 -0700

    logic added to determine info sent as obect based on user choice

--- Develop/app.js
+++ Develop/app.js
@@ -10,6 +10,9 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// Array holding all data
+const fullTeam = [];
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 const generateTeam = () => {
@@ -37,46 +40,63 @@ const generateTeam = () => {
         name: "role",
         choices: ["Manager", "Engineer", "Intern"],
       },
+      {
+        type: "input",
+        message: "Please enter your office number",
+        name: "office",
+        when: (answer) => answer.role === "Manager",
+      },
+      {
+        type: "input",
+        message: "Which school did you attend?",
+        name: "school",
+        when: (answer) => answer.role === "Intern",
+      },
+      {
+        type: "input",
+        message: "What is your GitHub username",
+        name: "git",
+        when: (answer) => answer.role === "Engineer",
+      },
+      {
+        type: "list",
+        message: "Add another employee?",
+        name: "continue",
+        choices: ["Yes", "No"],
+      },
     ])
     .then((response) => {
       if (response.role === "Manager") {
-        inquirer.prompt([
-          {
-            type: "input",
-            message: "Please enter your office number",
-            name: "office",
-          },
-        ]);
+        let roleChoice = new Manager(
+          response.name,
+          response.email,
+          response.id,
+          response.office
+        );
+        fullTeam.push(roleChoice);
       } else if (response.role === "Intern") {
-        inquirer.prompt([
-          {
-            type: "input",
-            message: "Which school do you attend?",
-            name: "school",
-          },
-        ]);
+        let roleChoice = new Intern(
+          response.name,
+          response.email,
+          response.id,
+          response.school
+        );
+        fullTeam.push(roleChoice);
       } else {
-        inquirer.prompt([
-          {
-            type: "input",
-            message: "What is your GitHub username? ",
-            name: "git",
-          },
-        ]);
+        let roleChoice = new Engineer(
+          response.name,
+          response.email,
+          response.id,
+          response.git
+        );
+        fullTeam.push(roleChoice);
+        if (response.continue === "Yes") {
+          generateTeam();
+        } else {
+          console.log("Yay!");
+        }
       }
-      const html = () => {
-        `<!DOCTYPE html>
-        <html lang="en">
-          <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Document</title>
-          </head>
-          <body>
-            Hello
-          </body>
-        </html>`;
-      };
+      console.log(fullTeam);
     });
 };
 generateTeam();
