32c07dc94e83cde76a6a7973f3fdbd6c802bec0d
Jacob Kriese <jkriese12@gmail.com>
2021-01-29 20:28:09 -0700

    Inquirer if logic added to prompt correct questions

--- Develop/app.js
+++ Develop/app.js
@@ -10,76 +10,73 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-const generate = () => {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          message: "Title of your project?",
-          name: "title",
-        },
-        {
-          type: "input",
-          message: "Please provide a description of your project",
-          name: "description",
-        },
-        {
-          type: "input",
-          message: "Please provide usage information for your project",
-          name: "usage",
-        },
-        {
-          type: "input",
-          message: "Please provide contribution guidelines",
-          name: "contribution",
-        },
-        {
-          type: "input",
-          message: "Please provide installation instructions",
-          name: "installation",
-        },
-        {
-          type: "input",
-          message: "Please provide test instructions",
-          name: "test",
-        },
-        {
-          type: "list",
-          message: "Which license was used for this project?",
-          name: "license",
-          choices: ["MIT", "Mozilla", "Perl", "None of the above"],
-        },
-        {
-          type: "input",
-          message: "Please enter your github username",
-          name: "gitQuestion",
-        },
-        {
-          type: "input",
-          message: "Please enter your email",
-          name: "email",
-        },
-      ])
-      
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+const generateTeam = () => {
+  inquirer
+    .prompt([
+      // Gathering all information for team member that will be constant
+      {
+        type: "input",
+        message: "Please type in your name",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "Please provide your email address",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "Please provide your employee ID #",
+        name: "id",
+      },
+      {
+        type: "list",
+        message: "What is your company role?",
+        name: "role",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+    ])
+    .then((response) => {
+      if (response.role === "Manager") {
+        inquirer.prompt([
+          {
+            type: "input",
+            message: "Please enter your office number",
+            name: "office",
+          },
+        ]);
+      } else if (response.role === "Intern") {
+        inquirer.prompt([
+          {
+            type: "input",
+            message: "Which school do you attend?",
+            name: "school",
+          },
+        ]);
+      } else {
+        inquirer.prompt([
+          {
+            type: "input",
+            message: "What is your GitHub username? ",
+            name: "git",
+          },
+        ]);
+      }
+      const html = () => {
+        `<!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Document</title>
+          </head>
+          <body>
+            Hello
+          </body>
+        </html>`;
+      };
+    });
+};
+generateTeam();
